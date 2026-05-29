cf95e90fd398ed00896915cd510e46a2946538d2
Fadhil Saheer <fadhilsaheer@gmail.com>
2026-05-29 06:01:10 +0530

    removed daemon process: proceeding with shell spawn approach

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,5 @@
 use std::env;
-use std::fs;
-use std::path;
-use std::path::Path;
 use std::process::Command;
-
-const PID_FILE: &str = "/tmp/shellrec.pid";
 
 fn main() {
     let args: Vec<String> = env::args().collect();
@@ -14,10 +9,7 @@ fn main() {
     }
 
     match args[1].as_str() {
-        "start" => start(&args),
-        "shell" => shell_start(),
-        "stop" => stop(),
-        "--daemon" => daemon(),
+        "start" => shell_start(),
         _ => help(),
     }
 }
@@ -36,44 +28,4 @@ fn shell_start() {
         .env("PS1", "(record) $ ")
         .status()
         .expect("Failed to run shell");
-}
-
-fn start(args: &Vec<String>) {
-    if path::Path::new(PID_FILE).exists() {
-        let pid = fs::read_to_string(PID_FILE)
-            .unwrap_or_default()
-            .trim()
-            .to_string();
-        eprintln!("Already running (PID: {}). Run 'stop' first", pid);
-        std::process::exit(1);
-    }
-
-    // spawn itself as background process
-    let exe = &args[0];
-    Command::new(exe)
-        .arg("--daemon")
-        .stdin(std::process::Stdio::null())
-        .stdout(std::process::Stdio::null())
-        .stderr(std::process::Stdio::null())
-        .spawn()
-        .expect("Failed to spawn shellrec daemon");
-
-    println!("Started shellrec");
-}
-
-fn stop() {
-    if !Path::new(PID_FILE).exists() {
-        eprintln!("shellrec not running. Start it first using 'start'");
-        std::process::exit(1);
-    }
-
-    fs::remove_file(PID_FILE).expect("Failed to remove PID file");
-    println!("daemon stopped");
-}
-
-fn daemon() {
-    let pid = std::process::id();
-    fs::write(PID_FILE, pid.to_string()).expect("Failed to write PID file");
-
-    loop {}
 }
